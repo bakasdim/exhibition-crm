ff86069e9c46b65589841f18262614298fc3f4dd
bakasdim <50336539+bakasdim@users.noreply.github.com>
2025-10-19 20:15:47 +0300

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Replace these with YOUR actual values from Supabase dashboard
-const supabaseUrl = 'https://xgpzjkjcqohebsiyofol.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhncHpqa2pjcW9oZWJzaXlvZm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzQ1MDYsImV4cCI6MjA3NjExMDUwNn0.Dlm26WcwP8vu01XlrQ15owcpt3fkhfS0U5R43cUFsgA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
